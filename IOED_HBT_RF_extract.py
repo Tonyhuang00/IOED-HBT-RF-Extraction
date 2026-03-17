e7b1cafc9d7ec3a3abbe02c80e979ebe85cd189e
Tonyhuang00 <141016993+Tonyhuang00@users.noreply.github.com>
2026-03-18 01:14:44 +0800

    Add files via upload

--- IOED_HBT_RF_extract.py
+++ IOED_HBT_RF_extract.py
@@ -44,8 +44,8 @@ if not check_password():
 
 st.title("📡 IOED HBT RF Extraction Tool (Function Only · Beta v2)")
 st.caption("""
-**Core Settings / 核心設定:** Using NumPy, Extraction Range = Plot Range (萃取範圍與圖表同步)
-**Beta v2.0:Fix Merge Problem and add Smith Chart function / 修正總結空白問題與新增Smith圖
+**Core Settings / 核心設定:** Using NumPy, Extraction Range = Plot Range (萃取範圍與圖表同步)\n
+**Beta v2.0**:Fix Merge Problem and add Smith Chart function / 修正總結空白問題與新增Smith圖
 * If all below 0dB, return None (若全頻段低於 0dB，回傳空值)
 * If cross 0dB before 50GHz, return cross section point (若在 50GHz 前跌破 0dB，回傳交匯點頻率)
 * If no cross section above 50GHz, then using single extrapolate or UIUC method depending on the slope. (若超過 50GHz 未跌破 0dB，則根據斜率提供外插法與 UIUC 平台法雙輸出)
